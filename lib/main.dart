744a7a04b033d0147f0915b44f2e733e6b83e5d3
unknown <leonyrpl@gmail.com>
2026-08-20 12:16:06 +0700

    LKM-2 tata letak

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: const Center(child: Text('Selamat datang')),
+        appBar: AppBar(
+          title: const Text('Koperasi Sekolah'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Buku Tulis'),
+
+              const SizedBox(height: 8),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Anggota: Rp3.000'),
+                  Text('Umum: Rp3.500'),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
